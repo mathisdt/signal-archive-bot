0071770c51e897bed36ad2fc985723fa8a9e89d4
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2023-12-25 09:56:22 +0100

    handle empty config variables gracefully

--- main.py
+++ main.py
@@ -66,7 +66,8 @@ def check_and_possibly_archive_media(timestamp, source, group_id, message, attac
                 if "success_reaction" not in config["signal"] or config["signal"]["success_reaction"]:
                     # green check mark
                     signal.sendGroupMessageReaction("\u2705", False, source, timestamp, group_id)
-                if "logging" in config and "success_number" in config["logging"]:
+                if ("logging" in config and "success_number" in config["logging"]
+                        and len(config["logging"]["success_number"]) > 0):
                     signal.sendMessage(
                         f"successfully archived a message from {group_name} with {len(attachments)} file(s)",
                         [], ["+" + config["logging"]["success_number"]])
@@ -74,7 +75,8 @@ def check_and_possibly_archive_media(timestamp, source, group_id, message, attac
                 if "error_reaction" not in config["signal"] or config["signal"]["error_reaction"]:
                     # red cross mark
                     signal.sendGroupMessageReaction("\u274C", False, source, timestamp, group_id)
-                if "logging" in config and "error_number" in config["logging"]:
+                if ("logging" in config and "error_number" in config["logging"]
+                        and len(config["logging"]["error_number"]) > 0):
                     signal.sendMessage(
                         f"could not archive a message from {group_name} with {len(attachments)} file(s): {exception}",
                         [], ["+" + config["logging"]["error_number"]])
